ecf6cc5beee4b2e678ba0a5c7d3fd7c0241b7430
Vinisim reis <108934754+KleitaoDaMassa@users.noreply.github.com>
2023-09-18 08:07:18 -0300

    aula dia 18/09

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
  mensagem = (msg) => alert(msg);
  soma = (a,b) => a + b;
+ sub = (a,b) => a - b;
+ mult = (a,b) => a * b;
+ div = (a,b) => a / b;
+ raiz = x => Math.sqrt(x);
+
+ // mensagem( soma ( 10, soma(8,2) ) );
+ // mensagem (soma ( 2,div(2,2) ) );
+ 
 
