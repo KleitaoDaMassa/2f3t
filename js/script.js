6094063d3ba1eeed26ba1e123b6d04cab247e0d7
Vinisim reis <108934754+KleitaoDaMassa@users.noreply.github.com>
2023-10-02 08:29:10 -0300

    aula dia 02/10

--- js/script.js
+++ js/script.js
@@ -4,8 +4,24 @@
  mult = (a,b) => a * b;
  div = (a,b) => a / b;
  raiz = x => Math.sqrt(x);
+ let a = "";
+ let b = "";
+ let valor = "";
+ let temPonto = false;
 
  // mensagem( soma ( 10, soma(8,2) ) );
  // mensagem (soma ( 2,div(2,2) ) );
  
+ //funções para mostrar númeroas no display
+  function escrever_display(num){
+    document.getElementById("resultado").value = num;
+
+  }
+
+  //função para receber as teclas numericas e o ponto
+  function digitando(tecla){
+    valor += tecla;
+    escrever_display(valor);
+
+  }
 
